19ded292e888f4730eea6e85e3ca6907e4a86c59
murphbra <murphbra@oregonstate.edu>
2021-06-04 17:22:20 -0600

    Update homework6.js

--- homework6.js
+++ homework6.js
@@ -28,3 +28,7 @@ app.get('/reset-table',function(req,res,next){
     })
   });
 });
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
