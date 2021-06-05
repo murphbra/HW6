e78f83b5a06aae54a7ae93defd697d4f86761a73
murphbra <murphbra@oregonstate.edu>
2021-06-04 19:13:37 -0600

    update

--- homework6.js
+++ homework6.js
@@ -13,10 +13,19 @@ var pool = mysql.createPool({
   password        : '5461',
   database        : 'cs290_murphbra'
 });
-
-  
 app.set('port', 5463)
 
+app.get('/',function(req,res,next){
+    var context = {};
+    mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
+      if(err){
+        next(err);
+        return;
+      }
+      context.results = JSON.stringify(rows);
+      res.render('home', context);
+    });
+  });
 
 app.get('/reset-table',function(req,res,next){
   var context = {};
@@ -35,6 +44,18 @@ app.get('/reset-table',function(req,res,next){
   });
 });
 
+app.use(function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.send('500 - Server Error');
+  });
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
